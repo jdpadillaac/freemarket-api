f91b6ff1f46ddd8c84767f7c163478d2e0d2726e
Jonathan <jonathan962911@gmail.com>
2020-06-21 23:31:28 -0500

    initial commit  :tada:

---
+++ src/main/java/com/freemarket/Freemarket.java
@@ -0,0 +1,13 @@
+package com.freemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Freemarket {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Freemarket.class, args);
+	}
+
+}
---
+++ src/test/java/com/freemarket/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
